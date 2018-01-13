5a955a99b27105daffd85c6ee9aa30b85c636842
Pawel Buderaski <pbuderaski@gmail.com>
2018-01-13 01:23:51 +0000

    Initial setup

---
+++ deploy.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-expressions */
+const FtpDeploy = require('ftp-deploy');
+const path = require('path');
+const ftpDeploy = new FtpDeploy();
+
+const config = {
+  username: 'pi',
+  password: '',
+  host: '192.168.1.10',
+  port: 21,
+  localRoot: path.join(__dirname, '/static'),
+  remoteRoot: '/home/pi/server/static',
+  include: ['*'],
+  exclude: [
+    '.git',
+    '.idea',
+    'tmp/*',
+    'build/*',
+    'node_modules',
+    'app',
+    'webpack*',
+  ],
+};
+
+ftpDeploy.on('uploading', data => {
+  data.totalFileCount; // total file count being transferred
+  data.transferredFileCount; // number of files transferred
+  data.percentComplete; // percent as a number 1 - 100
+  data.filename; // partial path with filename being uploaded
+});
+
+ftpDeploy.on('uploaded', console.log);
+
+ftpDeploy.deploy(config, err => {
+  if (err) console.log(err);
+  else console.log('finished');
+});
---
+++ realData.js
@@ -0,0 +1,371 @@
+const moment = require('moment');
+
+module.exports = (isLive, refreshTime) => ({
+  "items": [
+    {
+      "images": [
+        "https://manager.vidibuscloud.net/mediafiles/4214/2298/6215/71bb5ed0d77a01357160543d7edaf0a0/tennis-live_1024x576.jpg"
+      ],
+      "now": isLive,
+      "asset_version": 4,
+      "starts_at": refreshTime.format(),
+      "asset_uuid": "9a005bd0d7740135abd77054d2ab776f",
+      "title": "ITF Schwieberdingen: Court 1- Tag 2",
+      "ends_at": "2018-01-10T12:00:00+01:00",
+      "tags": {
+        "Sportart": [
+          "Tennis"
+        ],
+        "Wettbewerb, Event": [
+          "ITF Schwieberdingen"
+        ],
+        "Kanal": [
+          "German Masters Series"
+        ],
+        "Geschlecht": [
+          "männlich"
+        ],
+        "Austragungsort": [
+          "Schwieberdingen"
+        ],
+        "Saison (Liga) / Termin (Event)": [
+          "2018"
+        ],
+        "Art des Inhalts": [
+          "Live"
+        ]
+      },
+      "body": "*Sportdeutschland.TV* zeigt das Tennis ITF Turnier in Schwieberdingen vom 10.1-14.1 ab 10 Uhr live und on demand.",
+      "path": "/german-masters-series/itf-schwieberdingen-court-1-tag-2",
+      "ics": "https://sicher.sportdeutschland.tv/german-masters-series/itf-schwieberdingen-court-1-tag-2.ics"
+    },
+    {
+      "images": [
+        "http://manager.vidibus.net/mediafiles/3278/1295/6215/226dc8e0c6ff01356513543d7edaf0a0/slowenien-bild.jpg"
+      ],
+      "now": false,
+      "asset_version": 2,
+      "starts_at": refreshTime.add(1, 'hour').format(),
+      "asset_uuid": "64995960c7020135e4137054d2ab776f",
+      "title": "Länderspiel: Slowenien vs. Serbien - Spiel 2",
+      "ends_at": "2018-01-10T20:30:00+01:00",
+      "tags": {
+        "Sportart": [
+          "Handball"
+        ],
+        "Wettbewerb, Event": [
+          "Testspiel"
+        ],
+        "Kanal": [
+          "EM 2018 der Männer"
+        ],
+        "Geschlecht": [
+          "männlich"
+        ],
+        "Saison (Liga) / Termin (Event)": [
+          "2018"
+        ],
+        "Teams": [
+          "Serbien",
+          "Slowenien"
+        ],
+        "Art des Inhalts": [
+          "Live"
+        ],
+        "_geo": [
+          "DE"
+        ],
+        "_flags": [
+          "opener",
+          "highlight"
+        ],
+        "_opener": [
+          "Ab 18 Uhr live: Slowenien vs. Serbien - letzter Test vor der EM!"
+        ]
+      },
+      "flags": [
+        "noembed",
+        "opener",
+        "highlight"
+      ],
+      "body": "*Handball-Deutschland.TV* zeigt das zweite Länderspiel zwischen Slowenien und Serbien live und on demand.",
+      "path": "/em-maenner-2018/laenderspiel-slowenien-vs-serbien-spiel-2_2",
+      "ics": "https://sicher.sportdeutschland.tv/em-maenner-2018/laenderspiel-slowenien-vs-serbien-spiel-2_2.ics?alarm=true"
+    },
+    {
+      "images": [
+        "http://manager.vidibus.net/mediafiles/2958/8998/6215/b3ae8060b759013591c4543d7edaf0a0/13-goettingen-halle.jpg"
+      ],
+      "now": false,
+      "asset_version": 2,
+      "starts_at": refreshTime.add(1, 'hour').format(),
+      "asset_uuid": "956b9330c70d0135e44a7054d2ab776f",
+      "title": "DBBL: BG 74 Veilchen Ladies vs. SV Lions Halle",
+      "ends_at": "2018-01-10T22:00:00+01:00",
+      "tags": {
+        "Sportart": [
+          "Basketball"
+        ],
+        "Wettbewerb, Event": [
+          "Damen Basketball Bundesliga"
+        ],
+        "Kanal": [
+          "DBBL"
+        ],
+        "Geschlecht": [
+          "weiblich"
+        ],
+        "Saison (Liga) / Termin (Event)": [
+          "2017/18"
+        ],
+        "Teams": [
+          "BG 74 Veilchen Ladies",
+          "SV Halle LIONS"
+        ],
+        "Art des Inhalts": [
+          "Live"
+        ]
+      },
+      "body": "*Sportdeutschland.TV* zeigt die DBBL Partie BG 74 Veilchen Ladies vs. SV Lions Halle am 10. Januar ab 19 Uhr live und on demand.",
+      "path": "/dbbl/dbbl-bg-74-veilchen-ladies-vs-sv-lions-halle_2",
+      "ics": "https://sicher.sportdeutschland.tv/dbbl/dbbl-bg-74-veilchen-ladies-vs-sv-lions-halle_2.ics?alarm=true"
+    },
+    {
+      "images": [
+        "http://manager.vidibus.net/mediafiles/6335/7498/6215/d416cf60b946013431e2543d7edaf0a0/2017-01-14-dbbl-herner-tc-v-firebalss-bad-aibling.jpg"
+      ],
+      "now": false,
+      "asset_version": 2,
+      "starts_at": refreshTime.add(1, 'hour').format(),
+      "asset_uuid": "94db78b0c70d0135e4487054d2ab776f",
+      "title": "DBBL: Herner TC vs. TuS Bad Aibling Fireballs",
+      "ends_at": "2018-01-10T22:00:00+01:00",
+      "tags": {
+        "Sportart": [
+          "Basketball"
+        ],
+        "Wettbewerb, Event": [
+          "Damen Basketball Bundesliga"
+        ],
+        "Kanal": [
+          "DBBL"
+        ],
+        "Geschlecht": [
+          "weiblich"
+        ],
+        "Saison (Liga) / Termin (Event)": [
+          "2017/18"
+        ],
+        "Teams": [
+          "Herner TC",
+          "TuS Bad Aibling Fireballs"
+        ],
+        "Art des Inhalts": [
+          "Live"
+        ]
+      },
+      "body": "*Sportdeutschland.TV* zeigt die DBBL Partie Herner TC vs. TuS Bad Aibling Fireballs am 10.Januar ab 19 Uhr live und on demand.",
+      "path": "/dbbl/dbbl-herner-tc-vs-bad-aibling-fireballs_2",
+      "ics": "https://sicher.sportdeutschland.tv/dbbl/dbbl-herner-tc-vs-bad-aibling-fireballs_2.ics?alarm=true"
+    },
+    {
+      "images": [
+        "http://manager.vidibus.net/mediafiles/2496/3145/6215/167058c0b819013591e4543d7edaf0a0/13-keltern-marburg.jpg"
+      ],
+      "now": false,
+      "asset_version": 2,
+      "starts_at": refreshTime.add(1, 'hour').format(),
+      "asset_uuid": "34feb080c70c0135e43b7054d2ab776f",
+      "title": "DBBL: Rutronik Stars Keltern vs. BC Pharmaserv Marbug",
+      "ends_at": "2018-01-10T22:00:00+01:00",
+      "tags": {
+        "Sportart": [
+          "Basketball"
+        ],
+        "Wettbewerb, Event": [
+          "Damen Basketball Bundesliga"
+        ],
+        "Kanal": [
+          "DBBL"
+        ],
+        "Geschlecht": [
+          "weiblich"
+        ],
+        "Saison (Liga) / Termin (Event)": [
+          "2017/18"
+        ],
+        "Teams": [
+          "Rutronik Stars Keltern",
+          "BC Pharmaserv Marburg"
+        ],
+        "Art des Inhalts": [
+          "live"
+        ]
+      },
+      "body": "*Sportdeutschland.TV* zeigt die DBBL Partie am 10. Januar ab 19 Uhr live und on demand.",
+      "path": "/dbbl/dbbl-rutronik-stars-keltern-vs-bc-pharmaserv-marbug_2",
+      "ics": "https://sicher.sportdeutschland.tv/dbbl/dbbl-rutronik-stars-keltern-vs-bc-pharmaserv-marbug_2.ics?alarm=true"
+    },
+    {
+      "images": [
+        "http://manager.vidibus.net/mediafiles/9879/4323/6215/b3b2c230b759013591cf543d7edaf0a0/13-hannover-chemnitz.jpg"
+      ],
+      "now": false,
+      "asset_version": 2,
+      "starts_at": refreshTime.add(1, 'hour').format(),
+      "asset_uuid": "95ba0850c70d0135e44c7054d2ab776f",
+      "title": "DBBL: TK Hannover vs. ChemCats Chemnitz",
+      "ends_at": "2018-01-10T22:00:00+01:00",
+      "tags": {
+        "Sportart": [
+          "Basketball"
+        ],
+        "Wettbewerb, Event": [
+          "Damen Basketball Bundesliga"
+        ],
+        "Kanal": [
+          "DBBL"
+        ],
+        "Geschlecht": [
+          "weiblich"
+        ],
+        "Saison (Liga) / Termin (Event)": [
+          "2017/18"
+        ],
+        "Teams": [
+          "TK Hannover",
+          "ChemCats Chemnitz"
+        ],
+        "Art des Inhalts": [
+          "Live"
+        ]
+      },
+      "body": "*Sportdeutschland.TV* zeigt die DBBL Partie TK Hannover vs. ChemCats Chemnitz am 10.Januar ab 19 Uhr live und on demand.",
+      "path": "/dbbl/dbbl-tk-hannover-vs-chemcats-chemnitz_2",
+      "ics": "https://sicher.sportdeutschland.tv/dbbl/dbbl-tk-hannover-vs-chemcats-chemnitz_2.ics?alarm=true"
+    },
+    {
+      "images": [
+        "http://manager.vidibus.net/mediafiles/1141/8417/6215/3bd83ca0ad1701354f5a543d7edaf0a0/alpen-volleys-haching-2-bild.jpg"
+      ],
+      "now": false,
+      "asset_version": 2,
+      "starts_at": refreshTime.add(1, 'hour').format(),
+      "asset_uuid": "94c72e30d1f0013563707054d2ab776f",
+      "title": "VBL: HYPO TIROL AlpenVolleys Haching vs. VfB Friedrichshafen",
+      "ends_at": "2018-01-10T22:00:00+01:00",
+      "tags": {
+        "Sportart": [
+          "Volleyball"
+        ],
+        "Wettbewerb, Event": [
+          "1. Liga"
+        ],
+        "Kanal": [
+          "VBL"
+        ],
+        "Geschlecht": [
+          "männlich"
+        ],
+        "Saison (Liga) / Termin (Event)": [
+          "2017/18"
+        ],
+        "Teams": [
+          "HYPO TIROL AlpenVolleys Haching",
+          "VfB Friedrichshafen"
+        ],
+        "Art des Inhalts": [
+          "Live"
+        ],
+        "_flags": [
+          "opener",
+          "highlight"
+        ],
+        "_opener": [
+          "Ab 19 Uhr live: Souveräne Häfler kämpfen gegen Haching weiter um eine weiße Weste"
+        ]
+      },
+      "flags": [
+        "opener"
+      ],
+      "body": "*Sportdeutschland.TV* zeigt die Partie aus der Volleyball-Bundesliga der Herren zwischen den HYPO TIROL AlpenVolleys Haching und dem VfB Friedrichshafen live und on demand.",
+      "path": "/vbl/vbl-hypo-tirol-alpenvolleys-haching-vs-vfb-friedrichshafen_2",
+      "ics": "https://sicher.sportdeutschland.tv/vbl/vbl-hypo-tirol-alpenvolleys-haching-vs-vfb-friedrichshafen_2.ics?alarm=true"
+    },
+    {
+      "images": [
+        "http://manager.vidibus.net/mediafiles/7773/1470/6215/b3b38900b759013591d1543d7edaf0a0/13-wasserburg-heidelberg.jpg"
+      ],
+      "now": false,
+      "asset_version": 2,
+      "starts_at": refreshTime.add(1, 'hour').format(),
+      "asset_uuid": "94894080c70d0135e4467054d2ab776f",
+      "title": "DBBL: TSV 1880 Wasserburg vs. AXSE BasCats USC Heidelberg",
+      "ends_at": "2018-01-10T22:30:00+01:00",
+      "tags": {
+        "Sportart": [
+          "Basketball"
+        ],
+        "Wettbewerb, Event": [
+          "Damen Basketball Bundesliga"
+        ],
+        "Kanal": [
+          "DBBL"
+        ],
+        "Geschlecht": [
+          "weiblich"
+        ],
+        "Saison (Liga) / Termin (Event)": [
+          "2017/18"
+        ],
+        "Teams": [
+          "TSV 1880 Wasserburg",
+          "AXSE BasCats USC Heidelberg"
+        ],
+        "Art des Inhalts": [
+          "Live"
+        ]
+      },
+      "body": "*Sportdeutschland.TV* zeigt die DBBL Partie TSV 1880 Wasserburg vs. AXSE BasCats Heidelberg am 10.Januar ab 19:30 Uhr live und on demand.",
+      "path": "/dbbl/dbbl-tsv-1880-wasserburg-vs-axse-bascats-usc-heidelberg_2",
+      "ics": "https://sicher.sportdeutschland.tv/dbbl/dbbl-tsv-1880-wasserburg-vs-axse-bascats-usc-heidelberg_2.ics?alarm=true"
+    },
+    {
+      "images": [
+        "http://manager.vidibus.net/mediafiles/4214/2298/6215/71bb5ed0d77a01357160543d7edaf0a0/tennis-live.jpg"
+      ],
+      "now": false,
+      "asset_version": 2,
+      "starts_at": refreshTime.add(1, 'hour').format(),
+      "asset_uuid": "72525a60d77b0135abf07054d2ab776f",
+      "title": "ITF Schwieberdingen: Court 1- Tag 3",
+      "ends_at": "2018-01-11T22:00:00+01:00",
+      "tags": {
+        "Sportart": [
+          "Tennis"
+        ],
+        "Wettbewerb, Event": [
+          "ITF Schwieberdingen"
+        ],
+        "Kanal": [
+          "German Masters Series"
+        ],
+        "Geschlecht": [
+          "männlich"
+        ],
+        "Austragungsort": [
+          "Schwieberdingen"
+        ],
+        "Saison (Liga) / Termin (Event)": [
+          "2018"
+        ],
+        "Art des Inhalts": [
+          "Live"
+        ]
+      },
+      "body": "*Sportdeutschland.TV* zeigt das Tennis ITF Turnier in Schwieberdingen vom 10.1-14.1 ab 10 Uhr live und on demand.",
+      "path": "/german-masters-series/itf-schwieberdingen-court-1-tag-3_2",
+      "ics": "https://sicher.sportdeutschland.tv/german-masters-series/itf-schwieberdingen-court-1-tag-3_2.ics?alarm=true"
+    }
+  ]
+});
